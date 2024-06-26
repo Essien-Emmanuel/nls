a829cb629fa0e5b0dfb8f8021a4c80e2122590ef
Essien Emmanuel <essienemma300@gmail.com>
2024-06-26 17:05:38 +0100

    callback based solution for checking if is file

--- index.js
+++ index.js
@@ -5,10 +5,21 @@ const fs = require('fs');
 fs.readdir(process.cwd(), (error, filenames) => {
     if (error) console.log('error:: ', error);
     
-    // Bad code
+    const allStats = Array(filenames.length).fill(null);
+
     for (const filename of filenames) {
+        const index = filenames.indexOf(filename);
+
         fs.lstat(filename, (err, stats) => {
-            console.log(filename,  stats.isFile())
+            if (err) console.log(err)
+            allStats[index] = stats;
+
+            const ready = allStats.every((stats) => stats);
+            if (ready) {
+                allStats.forEach((stats, index) => {
+                    console.log(filenames[index], stats.isFile())
+                })
+            }
         })
     }
 });
